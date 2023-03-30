6d7e5aa9117e1048904db20f6e4b246557e0abae
Jean Fraga <jeandevjs@gmail.com>
2023-03-30 16:31:16 -0300

    solve challenge-tabuleiro-com-buracos

--- challenge-tabuleiro-com-buracos.cpp
+++ challenge-tabuleiro-com-buracos.cpp
@@ -4,105 +4,63 @@
 using namespace std;
 
 int main () {
-  int moves, result = 0; 
+  int moves, result = 0, moveNumber, x = 4, y = 3; 
   cin >> moves; 
   
-  int listMoves[moves];
-  for (int i = 0; i < moves; i++) cin >> listMoves[i];
-
-  /**Chessboard**/
-	int board[8][8] = {0}; 
-  
-  /** Black holes **/
-	board[2][2] = 1;
-  board[3][5] = 1;
-  board[4][1] = 1;
-  board[4][2] = 1;
-
-  int x = 4, y = 4, i = 0; 
-  while(moves--) {
-    
-    int a, b;
-    switch (listMoves[i]) {
+  while( cin >> moveNumber && moves--) {
+    switch (moveNumber) {
       case 1:
-        a = x + 1;
-        b = y + 2;
-        board[a][b];
+        x += 1;
+        y += 2;
         break;
 
       case 2:
-        a = x + 2;
-        b = y + 1;
-        board[a][b];
+        x += 2;
+        y += 1;
         break;
 
       case 3:
-        a = x + 2;
-        b = y - 1;
-        board[a][b];
+        x += 2;
+        y -= 1;
         break;
 
       case 4:
-        a = x + 1;
-        b = y - 2;  
-        board[a][b];
+        x += 1;  
+        y -= 2;
         break;
 
       case 5:
-        a = x - 1;
-        b = y - 2; 
-        board[a][b];
+        x -= 1;
+        y -= 2;
         break;
 
       case 6:
-        a = x - 2;
-        b = y - 1;
-        board[a][b];
+        x -= 2;
+        y -= 1;
         break;
 
       case 7:
-        a = x - 2;
-        b = y + 1;
-        board[a][b];
+        x -= 2;
+        y += 1;
         break;
 
       case 8:
-        a = x - 1;
-        b = y + 2;
-        board[a][b];
+        x -= 1;
+        y += 2;
         break;          
 
-    default:
-      cout << "Input not valid";
-      break;
+      default:
+        break;
     };
-    
-    cout << board[a][b] << ' ' << a << ' ' << b << endl; 
-    if (board[a][b] != 1 && board[a][b] != 0) {
-      cout << "entrei"; 
-      i += 1;
-      continue;
-    };
-
-    if (board[a][b] == -1) {
-      result += 1;
-      break;
-    };
-
-    x = a;
-    y = b;
     result += 1;
-    i += 1;
-  }
+      /** Black holes **/
+    bool isHole = (x == 1 && y == 3) || 
+                  (x == 2 && y == 3) || 
+                  (x == 2 && y == 5) || 
+                  (x == 5 && y == 4);    
+    if (isHole) break;
+  }; 
 
-  // for (int i = 0; i < 8; i++) {
-  //   for (int j = 0; j < 8; j++) {
-  //     cout << board[i][j] << ' ';
-  //   }
-  //   cout << endl;
-  // }
   cout << result; 
 	return 0; 
 }
-//5
-//1 8 2 5 3
