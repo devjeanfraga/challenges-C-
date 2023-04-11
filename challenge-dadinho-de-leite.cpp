f0d4bb346281f88a8e01b62af99973d7fbe2e609
Jean Fraga <jeandevjs@gmail.com>
2023-04-10 22:43:58 -0300

    challenge: dadinho de leite

--- challenge-dadinho-de-leite.cpp
+++ challenge-dadinho-de-leite.cpp
@@ -4,9 +4,12 @@
 using namespace std;
 int main () {
   int N, D, C; 
-  while (cin >> N >> D >> C ) {
-    if ( N != 0 && D != 0 && C != 0 ) cout << N*D*C << endl;
-    else break;
+  // while (cin >> N >> D >> C ) {
+  //   if ( N != 0 && D != 0 && C != 0 ) cout << N*D*C << endl;
+  //   else break;
+  // }; 
+  while (cin >> N >> D >> C && N && D && C) {
+    cout << N*D*C << endl;
   }; 
 	return 0; 
 }
