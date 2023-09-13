c68106bfab2871e0bf02db04fa002ef71d49bcf1
Jean Fraga <jeanfragoli395@gmail.com>
2023-09-13 10:41:10 -0300

    challenge snake

---
+++ challenge-snake.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  #include <iostream>
+using namespace std;
+
+int main() {
+  // Escreva seu código aqui
+  int qtyRow, qtyCol;
+  cin >> qtyRow >> qtyCol;
+
+  int** gameBoard;
+  gameBoard = new int* [qtyRow];
+  for (int i = 0; i < qtyRow)
+
+  
+  return 0;
+}
+	return 0; 
+};
